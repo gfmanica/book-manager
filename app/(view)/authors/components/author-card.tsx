858e1e1d9754fbf78a90b8222c4fe443c6da066a
gabrielmanica <gabriel.manica@ciss.com.br>
2025-05-04 18:33:03 -0300

    feat: adiciona altura fixa ao contêiner de informações do autor e categoria

--- app/(view)/authors/components/author-card.tsx
+++ app/(view)/authors/components/author-card.tsx
@@ -6,7 +6,7 @@ import { Author } from '@/types';
 export default function AuthorCard({ author }: { author: Author }) {
     return (
         <div className="overflow-hidden rounded-lg border bg-white shadow-md">
-            <div className="p-4">
+            <div className="p-4 h-20">
                 <h2 className="truncate text-lg font-semibold">
                     {author.name}
                 </h2>
